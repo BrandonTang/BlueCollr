96011c0fa320281433db3c9045655ec3f9e42d53
Vincent Wong <bluekiller57@gmail.com>
2017-11-14 15:11:30 -0500

    merged with master

--- app/jobs/forms.py
+++ app/jobs/forms.py
@@ -10,3 +10,9 @@ class CreateJobForm(Form):
     street_name = StringField('Street Name', validators=[DataRequired(), Length(1, 64)])
     zip_code = IntegerField('Zip Code', validators=[DataRequired(), Length(5, 5)])
     submit = SubmitField('Create Job')
+
+
+class ReviewJobForm(Form):
+    rating = IntegerField('Rating (1-5)', validators=[DataRequired(), Length(1, 1)])
+    review = StringField('Review', validators=[DataRequired(), Length(1, 500)])
+    submit = SubmitField('Submit Review')
