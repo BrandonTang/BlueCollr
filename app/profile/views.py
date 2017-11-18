1a83d37fd1da1cff04e869dc1ffc706d8bcfc75a
ssk1002 <sahir.karani@gmail.com>
2017-11-18 18:05:35 -0500

    picture uploads now working properly.

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,13 +1,13 @@
 from app import db
 from app.models import User, Job
 from ..profile import profile
-from ..profile.forms import (
-    EditForm
-)
+from ..profile.forms import EditForm
+
 
 from flask import render_template, current_app, redirect, request, url_for, flash, session
 from flask_login import login_required, login_user, logout_user, current_user
 from ..constants import status
+import os
 
 
 @profile.route('/<user_id>', methods=['GET', 'POST'])
@@ -15,8 +15,15 @@ from ..constants import status
 def view_profile(user_id):
     user = User.query.filter_by(id=user_id).first()
     jobs_completed = Job.query.filter_by(accepted_id=user_id, status=status.COMPLETED)
-    return render_template('profile/profile.html', user=user, jobs_completed=jobs_completed)
+    if user.picture_path is None:
+        pic_path = "/static/img/userpics/default_pic.png"
+    else:
+        pic_path = user.picture_path
+        print pic_path
+    return render_template('profile/profile.html', pic_path=pic_path, user=user, jobs_completed=jobs_completed)
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @profile.route('/<user_id>/edit', methods=['GET', 'POST'])
 @login_required
@@ -38,6 +45,18 @@ def edit_profile(user_id):
             current_user.last_name = form.last_name.data
         if form.email != user.email:
             current_user.email = form.email.data
+        if form.file.data is not None:
+            f = form.file.data
+            if f.mimetype == "image/png":
+                ext = ".png"
+            elif f.mimetype == "image/jpg":
+                ext = '.jpg'
+            elif f.mimetype == "image/jpeg":
+                ext = '.jpeg'
+            path = os.path.join(current_app.config['UPLOAD_FOLDER'], str(user_id) + "_pic" + ext)
+            print path
+            f.save(path)
+            current_user.picture_path = path[5:]
         db.session.commit()
 
         flash('User information successfully updated!')
